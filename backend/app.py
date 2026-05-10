7811c6d3aaf3aa09d62c64be5f4a2936e2f47cb0
Jay Solanki <SolankiJay1808@hotmail.com>
2026-05-10 21:25:19 +0530

    added temparture

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import serial
 import time
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
 # CHANGE THIS PORT
 PORT = 'COM3'
+API_KEY = '6c87f822ebdd5f83c409031881eee79b'
+CITY = 'Jaipur'
 
 # Connect Arduino
 arduino = serial.Serial(PORT, 9600, timeout=1)
@@ -20,33 +23,48 @@ def home():
 
 @app.route('/sensor-data')
 def sensor_data():
-    try:
-        # Read serial data
-        data = arduino.readline().decode('utf-8').strip()
 
-        print("Raw Data:", data)
+    try:
+
+        # READ ARDUINO DATA
+        data = arduino.readline().decode('utf-8').strip()
 
         values = data.split(',')
 
-        if len(values) != 4:
-            return jsonify({
-                "error": "Invalid data received",
-                "raw": data
-            })
+        # WEATHER API
+        weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+        weather_response = requests.get(weather_url).json()
+
+        temperature = weather_response['main']['temp']
+
+        humidity = weather_response['main']['humidity']
+
+        weather = weather_response['weather'][0]['main']
 
         response = {
+
             "ldr_left": values[0],
+
             "ldr_right": values[1],
+
             "servo_angle": values[2],
-            "voltage": values[3]
+
+            "voltage": values[3],
+
+            "temperature": temperature,
+
+            "humidity": humidity,
+
+            "weather": weather
         }
 
         return jsonify(response)
 
     except Exception as e:
+
         return jsonify({
             "error": str(e)
         })
-
 if __name__ == '__main__':
     app.run()
--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,119 @@
+const lightLabels = [];
+const lightData = [];
+
+const voltageLabels = [];
+const voltageData = [];
+
+// LIGHT CHART
+const lightChart = new Chart(
+    document.getElementById('lightChart'),
+    {
+        type: 'line',
+
+        data: {
+            labels: lightLabels,
+
+            datasets: [{
+                label: 'Light Intensity',
+
+                data: lightData,
+
+                borderWidth: 2
+            }]
+        },
+
+        options: {
+            responsive: true
+        }
+    }
+);
+
+// VOLTAGE CHART
+const voltageChart = new Chart(
+    document.getElementById('voltageChart'),
+    {
+        type: 'line',
+
+        data: {
+            labels: voltageLabels,
+
+            datasets: [{
+                label: 'Voltage',
+
+                data: voltageData,
+
+                borderWidth: 2
+            }]
+        },
+
+        options: {
+            responsive: true
+        }
+    }
+);
+
 async function fetchData(){
 
-    const response = await fetch('http://127.0.0.1:5000/sensor-data');
+    try{
 
-    const data = await response.json();
+        const response = await fetch('http://127.0.0.1:5000/sensor-data');
 
-    document.getElementById('ldrLeft').innerText = data.ldr_left;
+        const data = await response.json();
 
-    document.getElementById('ldrRight').innerText = data.ldr_right;
+        // UPDATE CARDS
+        document.getElementById('ldrLeft').innerText = data.ldr_left;
 
-    document.getElementById('angle').innerText = data.servo_angle;
+        document.getElementById('ldrRight').innerText = data.ldr_right;
 
-    document.getElementById('voltage').innerText = data.voltage;
+        document.getElementById('angle').innerText = data.servo_angle;
+
+        document.getElementById('voltage').innerText =
+            parseFloat(data.voltage).toFixed(2);
+        document.getElementById('temperature').innerText =
+    data.temperature + " °C";
+
+document.getElementById('humidity').innerText =
+    data.humidity + "%";
+
+document.getElementById('weather').innerText =
+    data.weather;
+
+        // TIME LABEL
+        const time = new Date().toLocaleTimeString();
+
+        // LIGHT GRAPH
+        lightLabels.push(time);
+
+        lightData.push(data.ldr_left);
+
+        // VOLTAGE GRAPH
+        voltageLabels.push(time);
+
+        voltageData.push(data.voltage);
+
+        // LIMIT GRAPH POINTS
+        if(lightLabels.length > 10){
+
+            lightLabels.shift();
+            lightData.shift();
+
+            voltageLabels.shift();
+            voltageData.shift();
+        }
+
+        // UPDATE CHARTS
+        lightChart.update();
+
+        voltageChart.update();
+
+    }
+
+    catch(error){
+
+        console.log(error);
+    }
 }
 
-setInterval(fetchData, 1000);
+fetchData();
+
+setInterval(fetchData,1000);
